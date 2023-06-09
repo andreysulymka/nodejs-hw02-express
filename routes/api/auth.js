1e24b897994389000d80a875555c04af57b63cbc
andreysulymka <andreysulymka5@gmail.com>
2023-06-09 19:23:46 +0300

    add token and login router

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const authController = require("../../controllers/auth")
 
-const {userRegisterSchema} = require("../../schemas/users");
+const { userRegisterSchema } = require("../../schemas/users");
+const {userLoginSchema} = require("../../schemas/users");
 
 const { validateBody } = require("../../decorators");
 
 const router = express.Router();
 
 router.post("/signup", validateBody(userRegisterSchema), authController.signup)
+router.post("/signin", validateBody(userLoginSchema), authController.signin)
 
 module.exports = router;
--- schemas/users.js
+++ schemas/users.js
@@ -1,13 +1,19 @@
 const Joi = require("joi");
 
 const userRegisterSchema = Joi.object({
-     password: Joi.string().required(),
+  password: Joi.string().required(),
   email: Joi.string().required(),
   subscription: Joi.string().required(),
   token: String
-})
+});
+
+const userLoginSchema = Joi.object({
+  password: Joi.string().required(),
+  email: Joi.string().required(),
+  token: String
+});
 
 module.exports = {
     userRegisterSchema,
-    
+    userLoginSchema
 };
